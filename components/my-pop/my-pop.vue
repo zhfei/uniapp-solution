077bc105569c0e5feb57857e830d4a07060dd262
zhfei <1174977961@qq.com>
2023-03-07 23:48:42 +0800

    other: 父子组件事件传递

---
+++ components/my-pop/my-pop.vue
@@ -0,0 +1,39 @@
+<template>
+	<view class="box">
+		<!-- :style动态绑定传的是对象 -->
+		<view class="alert" :style="{height: state?'200rpx':'0rpx'}"></view>
+		<button type="default" size="mini" @click="closeAlert">关闭</button>
+		<button type="default" size="mini" @click.native="show">本地事件监听</button>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-pop",
+		props: ['state'],
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			closeAlert() {
+				this.$emit('closeAlert', false)
+			},
+			show(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.box {
+	.alert {
+		height: 200rpx;
+		width: 200rpx;
+		background-color: hotpink;
+	}
+}
+</style>
--- pages/about/about.vue
+++ pages/about/about.vue
@@ -2,7 +2,8 @@
 	<view>
 		关于
 		<com-demo title="关于北极" subtitle="重大新闻-北极光"></com-demo>
-		
+		<button type="default" @click="openAlert">打开弹窗,当前状态{{state}}</button>
+		<my-pop :state="state" @closeAlert="closeAlert()"></my-pop>
 	</view>
 </template>
 
@@ -10,8 +11,16 @@
 	export default {
 		data() {
 			return {
-				
+				state: true
 			};
+		},
+		methods: {
+			openAlert() {
+				this.state = true
+			},
+			closeAlert(v) {
+				this.state = v
+			}
 		}
 	}
 </script>
