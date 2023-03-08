d0f3114fc077ee79058ad4fff1641c9b0e2c8e3a
zhfei <1174977961@qq.com>
2023-03-08 22:27:57 +0800

    other: 页面和应用的生命周期

---
+++ pages/life-circle/life-circle.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		生命周期
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		created() {
+			console.log('组件实例创建完成')
+		},
+		mounted() {
+			console.log('组件实例挂载到Dom树上完成')
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
