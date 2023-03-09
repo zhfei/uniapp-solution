ed81c27cc7ff199743b1fa6e22145709697028bb
zhfei <1174977961@qq.com>
2023-03-09 08:15:08 +0800

    style: 添加iconfont ttf图标，（小程序不支持）

--- pages/center/center.vue
+++ pages/center/center.vue
@@ -11,6 +11,19 @@
 				
 			}
 		},
+		onReady(v) {
+			uni.setNavigationBarTitle({
+				title:'中心广场'
+			})
+			uni.setNavigationBarColor({
+				frontColor:'#000000'
+			})
+			uni.showNavigationBarLoading()
+			
+			setTimeout(()=>{
+				uni.hideNavigationBarLoading()
+			}, 1500)
+		},
 		methods: {
 			
 		}
