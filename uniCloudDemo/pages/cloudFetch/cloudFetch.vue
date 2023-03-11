770089a7400ab4b136b0c7629d393e59e1f93a97
zhfei <1174977961@qq.com>
2023-03-11 13:13:29 +0800

    feat: 云数据库条件查询

---
+++ uniCloudDemo/pages/cloudFetch/cloudFetch.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		云查询
+		<view v-for="(item,index) in list" :key="item._id">
+			<text space='emsp' decode>姓名：{{item.name}} </text>
+			<text space='emsp' decode>年龄：{{item.age}} </text>
+			<text space='emsp' decode>学校：{{item.address}} </text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: []
+			}
+		},
+		onReady() {
+			uniCloud.callFunction({
+				name:'myCloudGet',
+				success: (res) => {
+					console.log(res)
+					this.list = res.result.data
+				}
+			})
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ uniCloudDemo/uniCloud-aliyun/cloudfunctions/myCloudGet/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const db = uniCloud.database()
+
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	
+	// doc： 根据id查询
+	// let res = await db.collection('User').doc('640b5a9228064a03b7aa1ac7').get()
+	// 限制条数
+	// let res = await db.collection('User').limit(5).get()
+	// // skip：跳过的条数，分页的话数字是页数*每页条数
+	// let res = await db.collection('User').limit(5).skip(5).get()
+	
+	// field:只返回声明的字段，_id默认返回
+	// let res = await db.collection('User').field({name: true}).get()
+	// orderBy: 排序字段+升序/降序类型
+	let res = await db.collection('User').orderBy('age','desc').get()
+	
+	
+	//返回数据给客户端
+	return res
+};
