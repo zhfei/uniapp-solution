e8d25f71c86cf02e39e4cbbd00f2eeacf43958a2
zhfei <1174977961@qq.com>
2023-03-12 23:16:24 +0800

    init：添加新闻APP

---
+++ Articles/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="content">
+		文章列表
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
