eb2649e7333ab959006a091ddb9956f8387c5ca5
zhfei <1174977961@qq.com>
2023-03-12 22:38:29 +0800

    feat：增加更新操作

---
+++ uniCloudDemo/pages/cloudUpdate/cloudUpdate.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<button @click="updateDB">点击更新云数据库</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			updateDB() {
+				uniCloud.callFunction({
+					name:'myCloudUpdate',
+					success: (res) => {
+						console.log(res)
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ uniCloudDemo/uniCloud-aliyun/cloudfunctions/myCloudUpdate/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const { link } = require("fs");
+
+const db = uniCloud.database()
+const dbCmd = db.command
+
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	
+	// 单条记录更新
+	// const res = await db.collection('User').doc('640bf773e766bb2975957423').update({
+	// 	phone: '88889999'
+	// })
+	
+	// 多条记录更新
+	// const res = await db.collection('User').where({
+	// 	_id: dbCmd.in(['640bf773e766bb2975957423','640be1bc28064a03b7bd833f'])
+	// }).update({
+	// 	phone: '88889999000'
+	// })
+	
+	// const res = await db.collection('User').where({
+	// 	name: /梅/ig
+	// }).update({
+	// 	address: '冬梅大桥旁，33号'
+	// })
+	
+	// 更新对象和数组
+	// const res = await db.collection('User').where({
+	// 	name: "张三"
+	// }).update({
+	// 	like:{
+	// 		0: "游泳2"
+	// 	},
+	// 	bestFrient:{
+	// 		name:"jack"
+	// 	}
+	// })
+	
+	
+	// set: 覆盖一个对象， update:更新局部字段
+	const res = await db.collection('User').where({
+		name: "张三"
+	}).update({
+		// dbCmd.inc(1)：自增加一
+		love: dbCmd.inc(1),
+		// dbCmd.unshift(["写代码","打游戏"]): 数组头部添加数据
+		like: dbCmd.unshift(["写代码","打游戏"]),
+		// dbCmd.set({}) 更新一个对象，参数为传入的一个对象
+		bestFrient: dbCmd.set({
+			name: '狗剩',
+			age: 12
+		})
+	})
+	
+	//返回数据给客户端
+	return res
+};
