c9b566779d177db6bd5d433f141b298989c10b67
zhfei <1174977961@qq.com>
2023-03-13 23:18:25 +0800

    feat: 添加新增页布局

--- Articles/pages/add/add.vue
+++ Articles/pages/add/add.vue
@@ -1,6 +1,19 @@
 <template>
-	<view>
-		
+	<view class="add">
+		<form action="submit">
+			<view class="item">
+				<input type="text" name='title' placeholder="请输入标题"></input>
+			</view>
+			<view class="item">
+				<input type="text" name='author' placeholder="请输入作者"></input>
+			</view>
+			<view class="item">
+				<textarea type="text" name='content' placeholder="请输入详细内容"></textarea>
+			</view>
+			<view class="item">
+				<button>提交</button>
+			</view>
+		</form>
 	</view>
 </template>
 
@@ -17,6 +30,20 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.add {
+	.item {
+		padding: 20rpx;
+		input, textarea {
+			border: 1rpx solid #eee;
+			height: 20px;
+			border-radius: 5px;
+			padding: 10rpx;
+		}
+		textarea {
+			height: 80px;
+			box-sizing: border-box;
+		}
+	}
+}
 </style>
